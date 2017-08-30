ae17f5b40e35e6deb1b4d04408d6ae62369ead25
diyu <694310292@qq.com>
2017-08-30 16:54:12 +0800

    add user following

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-    before_action :logged_in_user, only:[:index, :edit, :update, :destroy]
+    before_action :logged_in_user, only:[:index, :edit, :update, :destroy, :following, :followers]
     before_action :correct_user,   only:[:edit, :update]
     before_action :admin_user, only: :destroy
     def show
@@ -46,13 +46,27 @@ class UsersController < ApplicationController
         redirect_to users_url
     end
 
+    def following
+        @title = "Following"
+        @user = User.find_by(params[:id])
+        @users = @user.following.paginate(page: params[:page])
+        render 'show_follow'
+    end
+
+    def followers
+        @title = "Followers"
+        @user = User.find_by(params[:id])
+        @users = @user.followers.paginate(page: params[:page])
+        render 'show_follow'
+    end
+
     private
     def user_params
         params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
 
     # 确保用户已登录
-    
+
 
     # 确保是正确的用户
     def correct_user
--- config/application.rb
+++ config/application.rb
@@ -1,3 +1,4 @@
+require File.expand_path('../boot', __FILE__)
 require_relative 'boot'
 
 require 'rails/all'
@@ -10,6 +11,7 @@ module HelloApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+    config.action_view.embed_authenticity_token_in_remote_forms = true
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
